2ad33acd4995f95ec23361138f2e24276eccc340
Jie Zou <u6849083@anu.edu.au>
2020-09-19 09:23:13 +0800

    Fix up some UI design and fragment implement changing password page

---
+++ FrontEnd/Android/app/src/main/java/com/example/myapplication/EditProfile.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.myapplication.ui.dashboard.DashboardFragment;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+    public void back1(View v){
+        finish();
+
+
+    }
+}
