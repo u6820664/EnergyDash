5898b41894223faedd5ab268e91fb599b79f6178
Haonan Liu <u6762898@anu.edu.au>
2020-09-21 11:26:58 +0800

    add dashboard metrics data loading

--- FrontEnd/web/js/dashboard.js
+++ FrontEnd/web/js/dashboard.js
@@ -7,22 +7,22 @@
       var AudienceChart = new Chart(AudienceChartCanvas, {
         type: 'bar',
         data: {
-          labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+          labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
           datasets: [
             {
               type: 'line',
               fill: false,
-              data: [100, 230, 130, 140, 270, 140],
+              data: [100, 230, 130, 140, 270, 140, 100, 230, 130, 140, 270, 140],
               borderColor: '#ff4c5b'
             },
             {
               label: 'Offline Sales',
-              data: [100, 230, 340, 340, 260, 340],
+              data: [100, 230, 340, 340, 260, 340, 100, 230, 130, 140, 270, 140],
               backgroundColor: '#6640b2'
             },
             {
               label: 'Online Sales',
-              data: [130, 190, 250, 250, 190, 260],
+              data: [130, 190, 250, 250, 190, 260, 100, 230, 130, 140, 270, 140],
               backgroundColor: '#1cbccd'
             }
           ]
@@ -260,7 +260,7 @@
         labels: ["Mon","Tue","Wed","Thu","Fri","Sat","Sun","Mon","Tue","Wed","Thu","Fri","Sat","Sun","Mon","Tue","Wed","Thu","Fri","Sat","Sun","Mon","Tue","Wed","Thu","Fri","Sat","Sun","Mon","Tue","Wed","Thu"],
         datasets: [
           {
-            data: [26.00, 14.00, 22.00, 12.00, 23.00, 24.00, 27.00, 12.00, 28.00, 26.00, 12.50, 19.00, 18.00, 28.00, 28.00, 12.00, 25.00, 26.00, 18.00, 12.00, 20.00, 18.00, 27.00, 16.00, 28.00, 20.00, 21.00, 12.00, 20.00, 12.00, 12.00, 25.00],
+            data: [Math.random()*30, Math.random()*30,Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30, Math.random()*30],
             borderColor: [
               '#1faf47'
             ],
@@ -351,16 +351,16 @@
       });
     }
 
-    if ($("#task-chart").length) {
-      var taskChartCanvas = $("#task-chart").get(0).getContext("2d");
+    if ($("#task-chart1").length) {
+      var taskChartCanvas = $("#task-chart1").get(0).getContext("2d");
       var taskChart = new Chart(taskChartCanvas, {
         type: 'bar',
         data: {
           labels: ["Jan", "Feb", "Mar", "Apr", "May","Jun", "Jul", "Aug"],
           datasets: [{
               label: 'Profit',
-              data: [-3, -5, -5, 3, 4, -5, -1, 9],
-              backgroundColor: '#f83e37'
+              data: [Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10],
+              backgroundColor: 'orange'
             }
           ]
         },
@@ -432,8 +432,446 @@
           labels: ["Jan", "Feb", "Mar", "Apr", "May","Jun", "Jul", "Aug"],
           datasets: [{
             label: 'Profit',
-            data: [-3, -5, -5, 3, 4, -5, -1, 9],
-            backgroundColor: '#f83e37'
+            data: [Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10],
+            backgroundColor: 'lightblue'
+          }
+          ]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: true,
+          layout: {
+            padding: {
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0
+            }
+          },
+          scales: {
+            yAxes: [{
+              display: true,
+              gridLines: {
+                drawBorder: false,
+                color: '#f1f3f9',
+                zeroLineColor: '#f1f3f9'
+              },
+              ticks: {
+                display: true,
+                fontColor: "#9fa0a2",
+                fontSize: 10,
+                padding: 0,
+                stepSize: 10,
+                min: -10,
+                max: 10
+              }
+            }],
+            xAxes: [{
+              display: false,
+              stacked: false,
+              categoryPercentage: 1,
+              ticks: {
+                display: false,
+                beginAtZero: false,
+                display: true,
+                padding: 10,
+                fontSize: 11
+              },
+              gridLines: {
+                color: "rgba(0, 0, 0, 0)",
+                display: false
+              },
+              position: 'bottom',
+              barPercentage: 0.7
+            }]
+          },
+          legend: {
+            display: false
+          },
+          elements: {
+            point: {
+              radius: 0
+            }
+          }
+        }
+      });
+    }
+
+    if ($("#task-chart3").length) {
+      var taskChartCanvas = $("#task-chart3").get(0).getContext("2d");
+      var taskChart = new Chart(taskChartCanvas, {
+        type: 'bar',
+        data: {
+          labels: ["Jan", "Feb", "Mar", "Apr", "May","Jun", "Jul", "Aug"],
+          datasets: [{
+            label: 'Profit',
+            data: [Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10],
+            backgroundColor: 'red'
+          }
+          ]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: true,
+          layout: {
+            padding: {
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0
+            }
+          },
+          scales: {
+            yAxes: [{
+              display: true,
+              gridLines: {
+                drawBorder: false,
+                color: '#f1f3f9',
+                zeroLineColor: '#f1f3f9'
+              },
+              ticks: {
+                display: true,
+                fontColor: "#9fa0a2",
+                fontSize: 10,
+                padding: 0,
+                stepSize: 10,
+                min: -10,
+                max: 10
+              }
+            }],
+            xAxes: [{
+              display: false,
+              stacked: false,
+              categoryPercentage: 1,
+              ticks: {
+                display: false,
+                beginAtZero: false,
+                display: true,
+                padding: 10,
+                fontSize: 11
+              },
+              gridLines: {
+                color: "rgba(0, 0, 0, 0)",
+                display: false
+              },
+              position: 'bottom',
+              barPercentage: 0.7
+            }]
+          },
+          legend: {
+            display: false
+          },
+          elements: {
+            point: {
+              radius: 0
+            }
+          }
+        }
+      });
+    }
+
+    if ($("#task-chart4").length) {
+      var taskChartCanvas = $("#task-chart4").get(0).getContext("2d");
+      var taskChart = new Chart(taskChartCanvas, {
+        type: 'bar',
+        data: {
+          labels: ["Jan", "Feb", "Mar", "Apr", "May","Jun", "Jul", "Aug"],
+          datasets: [{
+            label: 'Profit',
+            data: [Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10],
+            backgroundColor: 'blue'
+          }
+          ]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: true,
+          layout: {
+            padding: {
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0
+            }
+          },
+          scales: {
+            yAxes: [{
+              display: true,
+              gridLines: {
+                drawBorder: false,
+                color: '#f1f3f9',
+                zeroLineColor: '#f1f3f9'
+              },
+              ticks: {
+                display: true,
+                fontColor: "#9fa0a2",
+                fontSize: 10,
+                padding: 0,
+                stepSize: 10,
+                min: -10,
+                max: 10
+              }
+            }],
+            xAxes: [{
+              display: false,
+              stacked: false,
+              categoryPercentage: 1,
+              ticks: {
+                display: false,
+                beginAtZero: false,
+                display: true,
+                padding: 10,
+                fontSize: 11
+              },
+              gridLines: {
+                color: "rgba(0, 0, 0, 0)",
+                display: false
+              },
+              position: 'bottom',
+              barPercentage: 0.7
+            }]
+          },
+          legend: {
+            display: false
+          },
+          elements: {
+            point: {
+              radius: 0
+            }
+          }
+        }
+      });
+    }
+
+    if ($("#task-chart5").length) {
+      var taskChartCanvas = $("#task-chart5").get(0).getContext("2d");
+      var taskChart = new Chart(taskChartCanvas, {
+        type: 'bar',
+        data: {
+          labels: ["Jan", "Feb", "Mar", "Apr", "May","Jun", "Jul", "Aug"],
+          datasets: [{
+            label: 'Profit',
+            data: [Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10],
+            backgroundColor: 'orange'
+          }
+          ]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: true,
+          layout: {
+            padding: {
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0
+            }
+          },
+          scales: {
+            yAxes: [{
+              display: true,
+              gridLines: {
+                drawBorder: false,
+                color: '#f1f3f9',
+                zeroLineColor: '#f1f3f9'
+              },
+              ticks: {
+                display: true,
+                fontColor: "#9fa0a2",
+                fontSize: 10,
+                padding: 0,
+                stepSize: 10,
+                min: -10,
+                max: 10
+              }
+            }],
+            xAxes: [{
+              display: false,
+              stacked: false,
+              categoryPercentage: 1,
+              ticks: {
+                display: false,
+                beginAtZero: false,
+                display: true,
+                padding: 10,
+                fontSize: 11
+              },
+              gridLines: {
+                color: "rgba(0, 0, 0, 0)",
+                display: false
+              },
+              position: 'bottom',
+              barPercentage: 0.7
+            }]
+          },
+          legend: {
+            display: false
+          },
+          elements: {
+            point: {
+              radius: 0
+            }
+          }
+        }
+      });
+    }
+
+    if ($("#task-chart6").length) {
+      var taskChartCanvas = $("#task-chart6").get(0).getContext("2d");
+      var taskChart = new Chart(taskChartCanvas, {
+        type: 'bar',
+        data: {
+          labels: ["Jan", "Feb", "Mar", "Apr", "May","Jun", "Jul", "Aug"],
+          datasets: [{
+            label: 'Profit',
+            data: [Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10],
+            backgroundColor: 'lightblue'
+          }
+          ]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: true,
+          layout: {
+            padding: {
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0
+            }
+          },
+          scales: {
+            yAxes: [{
+              display: true,
+              gridLines: {
+                drawBorder: false,
+                color: '#f1f3f9',
+                zeroLineColor: '#f1f3f9'
+              },
+              ticks: {
+                display: true,
+                fontColor: "#9fa0a2",
+                fontSize: 10,
+                padding: 0,
+                stepSize: 10,
+                min: -10,
+                max: 10
+              }
+            }],
+            xAxes: [{
+              display: false,
+              stacked: false,
+              categoryPercentage: 1,
+              ticks: {
+                display: false,
+                beginAtZero: false,
+                display: true,
+                padding: 10,
+                fontSize: 11
+              },
+              gridLines: {
+                color: "rgba(0, 0, 0, 0)",
+                display: false
+              },
+              position: 'bottom',
+              barPercentage: 0.7
+            }]
+          },
+          legend: {
+            display: false
+          },
+          elements: {
+            point: {
+              radius: 0
+            }
+          }
+        }
+      });
+    }
+
+    if ($("#task-chart7").length) {
+      var taskChartCanvas = $("#task-chart7").get(0).getContext("2d");
+      var taskChart = new Chart(taskChartCanvas, {
+        type: 'bar',
+        data: {
+          labels: ["Jan", "Feb", "Mar", "Apr", "May","Jun", "Jul", "Aug"],
+          datasets: [{
+            label: 'Profit',
+            data: [Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10],
+            backgroundColor: 'red'
+          }
+          ]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: true,
+          layout: {
+            padding: {
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0
+            }
+          },
+          scales: {
+            yAxes: [{
+              display: true,
+              gridLines: {
+                drawBorder: false,
+                color: '#f1f3f9',
+                zeroLineColor: '#f1f3f9'
+              },
+              ticks: {
+                display: true,
+                fontColor: "#9fa0a2",
+                fontSize: 10,
+                padding: 0,
+                stepSize: 10,
+                min: -10,
+                max: 10
+              }
+            }],
+            xAxes: [{
+              display: false,
+              stacked: false,
+              categoryPercentage: 1,
+              ticks: {
+                display: false,
+                beginAtZero: false,
+                display: true,
+                padding: 10,
+                fontSize: 11
+              },
+              gridLines: {
+                color: "rgba(0, 0, 0, 0)",
+                display: false
+              },
+              position: 'bottom',
+              barPercentage: 0.7
+            }]
+          },
+          legend: {
+            display: false
+          },
+          elements: {
+            point: {
+              radius: 0
+            }
+          }
+        }
+      });
+    }
+
+    if ($("#task-chart8").length) {
+      var taskChartCanvas = $("#task-chart8").get(0).getContext("2d");
+      var taskChart = new Chart(taskChartCanvas, {
+        type: 'bar',
+        data: {
+          labels: ["Jan", "Feb", "Mar", "Apr", "May","Jun", "Jul", "Aug"],
+          datasets: [{
+            label: 'Profit',
+            data: [Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10, Math.random()*20-10],
+            backgroundColor: 'blue'
           }
           ]
         },
