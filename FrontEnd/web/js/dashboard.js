a7549d9c128e6d53bbd3fe4a1da6efc4f665198e
Han Xiu <u6820664@anu.edu.au>
2020-10-09 21:00:00 +1100

    Fix metrics data loading without success.

--- FrontEnd/web/js/dashboard.js
+++ FrontEnd/web/js/dashboard.js
@@ -910,78 +910,7 @@
 function PerRefresh() {
 
 
-    if ($("#task-chart5").length) {
-        var taskChartCanvas = $("#task-chart5").get(0).getContext("2d");
-        var taskChart = new Chart(taskChartCanvas, {
-            type: 'line',
-            data: {
-                labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug"],
-                datasets: [{
-                    label: 'Profit',
-                    data: [Math.random() * 20 - 10, Math.random() * 20 - 10, Math.random() * 20 - 10, Math.random() * 20 - 10, Math.random() * 20 - 10, Math.random() * 20 - 10, Math.random() * 20 - 10, Math.random() * 20 - 10],
-                    backgroundColor: 'orange'
-                }
-                ]
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: true,
-                layout: {
-                    padding: {
-                        left: 0,
-                        right: 0,
-                        top: 0,
-                        bottom: 0
-                    }
-                },
-                scales: {
-                    yAxes: [{
-                        display: true,
-                        gridLines: {
-                            drawBorder: false,
-                            color: '#f1f3f9',
-                            zeroLineColor: '#f1f3f9'
-                        },
-                        ticks: {
-                            display: true,
-                            fontColor: "#9fa0a2",
-                            fontSize: 10,
-                            padding: 0,
-                            stepSize: 10,
-                            min: -10,
-                            max: 10
-                        }
-                    }],
-                    xAxes: [{
-                        display: false,
-                        stacked: false,
-                        categoryPercentage: 1,
-                        ticks: {
-                            display: false,
-                            beginAtZero: false,
-                            display: true,
-                            padding: 10,
-                            fontSize: 11
-                        },
-                        gridLines: {
-                            color: "rgba(0, 0, 0, 0)",
-                            display: false
-                        },
-                        position: 'bottom',
-                        barPercentage: 0.7
-                    }]
-                },
-                legend: {
-                    display: false
-                },
-                elements: {
-                    point: {
-                        radius: 0
-                    }
-                }
-            }
-        });
-    }
+
 
     if ($("#task-chart6").length) {
         var taskChartCanvas = $("#task-chart6").get(0).getContext("2d");
